ab282fece69685716e5ca88b6d1d7628740b23d3
Tiago Simões <tiagojfsimoes@gmail.com>
2024-05-24 11:25:17 +0100

    refact: Changes the database queries
    Instead of fetching the data with the category id, fetches the data with the category name.

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,34 @@ const pool = require('../config/db'); // Import your database connection pool
 class User {
   static async findById(id) {
     try {
-      const result = await pool.query('SELECT * FROM public.usr_data WHERE id = $1', [id]);
+      const result = await pool.query(`
+        SELECT
+          u.id,
+          u.first_names,
+          u.last_names,
+          u.birth_date,
+          u.phone_number,
+          u.prefix_phone_number,
+          u.address,
+          u.zipcode,
+          u.city,
+          u.aboutme,
+          u.username,
+          u.email,
+          u.active,
+          u.photo,
+          u.profile_id,
+          u.language_id,
+          u.employe_number,
+          c.name AS category_name 
+        FROM
+          usr_data u
+        JOIN
+          category c
+        ON
+          u.category_id = c.id
+        WHERE 
+          u.id = $1`, [id]);
       return result.rows.length > 0 ? result.rows[0] : null; // Return the user object or null if not found
     } catch (err) {
       console.error('Error finding user by ID:', err.stack);
@@ -15,7 +42,33 @@ class User {
 
   static async findAll() {
     try {
-      const result = await pool.query('SELECT * FROM public.usr_data');
+      const result = await pool.query(`
+        SELECT 
+          u.id,
+          u.first_names,
+          u.last_names,
+          u.birth_date,
+          u.phone_number,
+          u.prefix_phone_number,
+          u.address,
+          u.zipcode,
+          u.city,
+          u.aboutme,
+          u.username,
+          u.email,
+          u.active,
+          u.photo,
+          u.profile_id,
+          u.language_id,
+          u.employe_number,
+          c.name AS category_name
+        FROM 
+          usr_data u
+        JOIN 
+          category c 
+        ON
+          u.category_id = c.id
+        `);
       return result.rows;
     } catch (err) {
       console.error('Error fetching all users:', err.stack);
@@ -26,9 +79,23 @@ class User {
   static async findActive() {
     try {
       const result = await pool.query(`
-        SELECT id, first_names, last_names, phone_number, prefix_phone_number, email, photo, category_id
-        FROM public.usr_data
-        WHERE active = true
+        SELECT
+          u.id,
+          u.first_names,
+          u.last_names,
+          u.phone_number,
+          u.prefix_phone_number,
+          u.email,
+          u.photo,
+          c.name AS category_name
+        FROM
+          usr_data u
+        JOIN
+          category c
+        ON 
+          u.category_id = c.id
+        WHERE 
+          active = true
       `);
       return result.rows;
     } catch (err) {
