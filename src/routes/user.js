e84a6287956302f6f32b99ce4df8425670c2b436
Tiago Simões <tiagojfsimoes@gmail.com>
2024-06-21 09:41:05 +0100

    refactor: removed comments

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 // Define user routes
-router.get('/details/:id', userController.getDetails); //TODO: Change from post to get -> this route does not create anything, gets the info according to the user ID
+router.get('/details/:id', userController.getDetails);
 router.get('/', userController.getAll);
 router.get('/active', userController.getActive);
 router.patch('/update/:id', userController.update);
